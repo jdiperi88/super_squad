dd3f5773ba0ca2764e5ea3afd4c53da878f3ef32
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-13 22:50:07 -0500

    added first action creator

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import rootReducer from './reducers'
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import {addCharacterById} from './actions/actions'
 
 const store = createStore(rootReducer);
+
+store.subscribe(()=> console.log(store.getState()))
 console.log(store.getState())
+store.dispatch(addCharacterById(3))
 ReactDOM.render(
     <Provider store={store} >
         <App />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import characters_json from '../data/characters.json';
-
+import {ADD_CHARACTER} from '../actions/actions'
 function characters(state = characters_json, action) {
     switch(action.type) {
+        case ADD_CHARACTER:
+            let characters = state.filter(item => item.id !== action.id);
+            return characters;
         default: 
             return state;
     }
