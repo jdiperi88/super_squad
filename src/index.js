965ef8684c672691dc2aa84ab6c8053e32ce531d
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-13 22:35:03 -0500

    added redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import rootReducer from './reducers'
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+console.log(store.getState())
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
