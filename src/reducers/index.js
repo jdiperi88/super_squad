9028d1bc1e462eab08d6b075bb866e56b301b566
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-14 23:51:09 -0500

    added heroes reducer, characters reducer, and a helper reducer file.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,10 @@
-import characters_json from '../data/characters.json';
-import {ADD_CHARACTER} from '../actions/actions'
-function characters(state = characters_json, action) {
-    switch(action.type) {
-        case ADD_CHARACTER:
-            let characters = state.filter(item => item.id !== action.id);
-            return characters;
-        default: 
-            return state;
-    }
-}
+import {combineReducers } from 'redux';
+import characters from './characters_reducers';
+import heroes from './heroes_reducer';
 
-export default characters
+
+const rootReducer = combineReducers({
+    characters,
+    heroes
+})
+export default rootReducer;
