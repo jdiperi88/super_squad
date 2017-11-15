c8d26d0672054e0b8c2344db10039916cebe018f
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-15 10:42:24 -0500

    added ux for the characterlist componenet

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import CharacterList from './CharacterList';
+import '../styles/index.css'
 class App extends Component {
     render(){
         return ( 
-            <div>
+            <div className="App">
                 <h2>Super Squads</h2>
                 <CharacterList />
             </div>
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -10,13 +10,15 @@ class CharacterList extends Component{
         return (
             <div>
                 <h4>characters</h4>
-                <ul>
+                <ul className='list-group'>
                     {
                         this.props.characters.map(character =>{
-                            return <li key={character.id}> 
-                                        <div>{character.name}</div>
-                                        <div onClick={()=>{
-                                            return this.props.addCharacterById(character.id)
+                            return <li key={character.id} className='list-group-item'> 
+                                        <div className='list-item'>{character.name}</div>
+                                        <div 
+                                            className='list-item right-button' 
+                                            onClick={()=>{
+                                                return this.props.addCharacterById(character.id)
                                         }}>
                                             +
                                         </div>
